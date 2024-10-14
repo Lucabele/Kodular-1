8b12f577e02066d93548918c882b5b2a49c57f21
lucas <lucas@LUCAS-ELEUTERIO>
2024-10-14 02:47:18 -0300

    Adicionar arquivos de projeto.

---
+++ Kodular-1/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Kodular_1
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Kodular-1/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿namespace Kodular_1
+{
+    public partial class MainPage : ContentPage
+    {
+        
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+
+
+            try {
+
+                Double etanol = Convert.ToDouble(txt_etanol.Text);
+                Double gasolina = Convert.ToDouble(txt_gasolina.Text);
+
+                String msg = "";
+
+                if (etanol <= (gasolina * 0.7))
+                {
+
+                    msg = "O Etanol esta compensando.";
+                }
+
+                else
+                {
+                    msg = "A Gasolina esta compensando.";
+                }
+
+                DisplayAlert("Calculado", msg, "Ok");
+
+            } catch (Exception ex)
+            {
+
+                DisplayAlert("Ops", ex.Message, "Fechar");
+            }
+
+
+
+        } // Fecha método
+    } // Fecha Class
+
+} // Fecha nameSpace
